efc3162be432afe12171492d8fb07dc403abe87c
Stina Persson <stpe1901@student.miun.se>
2025-02-19 14:30:08 +0100

    funkton för att hämta poster skapad

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,27 +1,58 @@
 /*Komponenet för att  läsa in och skriva ut poster från api */
+
+import { useState } from "react";
+import { useEffect } from "react";
+
+  //interface för svar från api
+interface Post {
+  _id: string,
+  title: string, 
+  author: string, 
+  postText: string, 
+  created: string
+}; 
+
 const BlogPost = () => {
-    const GetPosts = async () => {
+  
+  //state för bloggpost 
+  const [posts, setPosts] = useState<Post [] | []>([]); 
+
+      //useEffect 
+      useEffect(() => {
+        getPosts(); 
+    }, [])
+
+  const getPosts = async () => {
         
-        try {
-            let response = await fetch("https://hapiblog.onrender.com/posts"); 
+      try {
+          let response = await fetch("https://hapiblog.onrender.com/posts"); 
 
-            if(response.ok) {
-                let data = await response.json(); 
-                console.log(data); 
-            } else {
-                console.log("Fel vid hämtning av data"); 
-            }
+          if(response.ok) {
+              let data = await response.json(); 
+              setPosts(data); 
+                
+          } else {
+              console.log("Fel vid hämtning av data"); 
+          }
 
-        } catch(error) {
-            console.error(error); 
-        }
-    }
+      } catch(error) {
+          console.error(error); 
+      }
+  }
 
   return (
-
-    <div>
-      <button onClick={GetPosts}>Klicka!</button>
-    </div>
+    <>
+    {
+      posts.map((post) => (
+        <div className="blogpostDiv" key={post._id}>
+          <h3>{post.title}</h3>
+          <em>{post.created}</em>
+          <p>{post.author}</p>
+          <article>{post.postText}</article> 
+        </div>
+      ))
+    }
+    </>
   )
 }
 
